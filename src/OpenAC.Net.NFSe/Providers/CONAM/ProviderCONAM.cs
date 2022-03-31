a94b493e0ed8be5a54a7e6c658b07cf664e0bdec
Adriano Trentim Augusto <adriano@divsoft.com.br>
2022-03-31 17:29:18 -0300

    Correções nova verão CONAM

--- src/OpenAC.Net.NFSe/Providers/CONAM/ProviderCONAM.cs
+++ src/OpenAC.Net.NFSe/Providers/CONAM/ProviderCONAM.cs
@@ -545,7 +545,7 @@ namespace OpenAC.Net.NFSe.Providers
             MensagemErro2(retornoWebservice, xmlRet);
             if (retornoWebservice.Erros.Any()) return;
 
-            var xmlElement = xmlRet.ElementAnyNs("Info");
+            var xmlElement = xmlRet.ElementAnyNs("SDT_IMPRESSAO_OUT");
 
             var listaNfse = xmlElement.ElementAnyNs("Lista_Notas");
             if (listaNfse == null)
@@ -604,7 +604,7 @@ namespace OpenAC.Net.NFSe.Providers
             MensagemErro2(retornoWebservice, xmlRet);
             if (retornoWebservice.Erros.Any()) return;
 
-            var xmlElement = xmlRet.ElementAnyNs("Info");
+            var xmlElement = xmlRet.ElementAnyNs("SDT_IMPRESSAO_OUT");
 
             var listaNfse = xmlElement.ElementAnyNs("Lista_Notas");
             if (listaNfse == null)
@@ -789,10 +789,10 @@ namespace OpenAC.Net.NFSe.Providers
 
         private static void MensagemErro2(RetornoWebservice retornoWs, XContainer xmlRet)
         {
-            if (xmlRet.ElementAnyNs("Info").ElementAnyNs("Sucesso").GetValue<bool>())
+            if (xmlRet.ElementAnyNs("SDT_IMPRESSAO_OUT").ElementAnyNs("Sucesso").GetValue<bool>())
                 return;
 
-            var mensagem = xmlRet.ElementAnyNs("Info").ElementAnyNs("Message");
+            var mensagem = xmlRet.ElementAnyNs("SDT_IMPRESSAO_OUT").ElementAnyNs("Message");
 
             var evento = new Evento
             {
