04189950417f059137f996aadefa9b864c9af511
Transis ERP <felipe@transis.com.br>
2022-08-29 14:11:35 -0300

    Update IPMServiceClient.cs
    Revertido para Metodo generico de upload

--- src/OpenAC.Net.NFSe/Providers/IPM/IPMServiceClient.cs
+++ src/OpenAC.Net.NFSe/Providers/IPM/IPMServiceClient.cs
@@ -4,7 +4,7 @@
 // Created          : 30-05-2022
 //
 // Last Modified By : Felipe Silveira (Transis Software)
-// Last Modified On : 27-08-2022
+// Last Modified On : 29-08-2022
 //
 // ***********************************************************************
 // <copyright file="IPMServiceClient.cs" company="OpenAC .Net">
@@ -51,9 +51,9 @@ namespace OpenAC.Net.NFSe.Providers
 
         #region Methods
 
-        public string EnviarSincrono(string cabec, string msg) => Upload("", msg);
+        public string EnviarSincrono(string cabec, string msg) => UploadIPM("", msg);
 
-        public string ConsultarLoteRps(string cabec, string msg) => Upload("", msg);
+        public string ConsultarLoteRps(string cabec, string msg) => UploadIPM("", msg);
 
         public string ConsultarNFSeRps(string cabec, string msg) => throw new NotImplementedException();
 
@@ -81,6 +81,91 @@ namespace OpenAC.Net.NFSe.Providers
             var authenticationString = string.Concat(Provider.Configuracoes.WebServices.Usuario, ":", Provider.Configuracoes.WebServices.Senha);
             var base64EncodedAuthenticationString = Convert.ToBase64String(Encoding.ASCII.GetBytes(authenticationString));
             return "Basic " + base64EncodedAuthenticationString;
+        }
+
+        protected string UploadIPM(string action, string message)
+        {
+            var url = Url;
+
+            try
+            {
+                var auth = Authentication();
+                var fileName = $"{DateTime.Now:yyyyMMddssfff}_{PrefixoEnvio}_envio.xml";
+                var arquivoEnvio = Path.Combine(Path.GetTempPath(), fileName);
+                File.WriteAllText(arquivoEnvio, message);
+
+                //Identificate separator
+                string boundary = "---------------------------" + DateTime.Now.Ticks.ToString("x");
+                //Encoding
+                byte[] boundarybytes = System.Text.Encoding.ASCII.GetBytes("\r\n--" + boundary + "\r\n");
+
+                //Creation and specification of the request
+                HttpWebRequest wr = (HttpWebRequest)WebRequest.Create(url); //sVal is id for the webService
+                wr.ContentType = "multipart/form-data; boundary=" + boundary;
+                wr.Method = "POST";
+                wr.KeepAlive = true;
+                wr.Credentials = System.Net.CredentialCache.DefaultCredentials;
+
+                wr.Headers.Add("Authorization: " + auth); //AUTHENTIFICATION END
+                Stream rs = wr.GetRequestStream();
+
+
+                //string formdataTemplate = "Content-Disposition: form-data; name=\"{0}\"\r\n\r\n{1}"; //For the POST's format
+
+                //Writting of the file
+                rs.Write(boundarybytes, 0, boundarybytes.Length);
+                byte[] formitembytes = System.Text.Encoding.UTF8.GetBytes(arquivoEnvio);
+                rs.Write(formitembytes, 0, formitembytes.Length);
+
+                rs.Write(boundarybytes, 0, boundarybytes.Length);
+
+                string headerTemplate = $"Content-Disposition: form-data; name=\"file\"; filename=\"{fileName}\"\r\nContent-Type: text/xml\r\n\r\n";
+                byte[] headerbytes = System.Text.Encoding.UTF8.GetBytes(headerTemplate);
+                rs.Write(headerbytes, 0, headerbytes.Length);
+
+                FileStream fileStream = new FileStream(arquivoEnvio, FileMode.Open, FileAccess.Read);
+                byte[] buffer = new byte[4096];
+                int bytesRead = 0;
+                while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) != 0)
+                {
+                    rs.Write(buffer, 0, bytesRead);
+                }
+                fileStream.Close();
+
+                byte[] trailer = System.Text.Encoding.ASCII.GetBytes("\r\n--" + boundary + "--\r\n");
+                rs.Write(trailer, 0, trailer.Length);
+                rs.Close();
+                rs = null;
+
+                WebResponse wresp = null;
+                try
+                {
+                    //Get the response
+                    wresp = wr.GetResponse();
+                    Stream stream2 = wresp.GetResponseStream();
+                    StreamReader reader2 = new StreamReader(stream2);
+                    EnvelopeRetorno = reader2.ReadToEnd();
+                }
+                catch (Exception ex)
+                {
+                    string s = ex.Message;
+                }
+                finally
+                {
+                    if (wresp != null)
+                    {
+                        wresp.Close();
+                        wresp = null;
+                    }
+                    wr = null;
+                }
+
+                return EnvelopeRetorno;
+            }
+            finally
+            {
+                Url = url;
+            }
         }
 
         #endregion Methods
