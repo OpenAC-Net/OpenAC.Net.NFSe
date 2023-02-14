327cf025afc4676a66ffccdaa76b8bf9bd7ed284
Felipe Silveira <transis@gmail.com>
2023-02-14 09:59:51 -0300

    CNAE de arcordo com o schema para o PRONIM203

--- src/OpenAC.Net.NFSe.Demo/FormMain.cs
+++ src/OpenAC.Net.NFSe.Demo/FormMain.cs
@@ -507,7 +507,7 @@ namespace OpenAC.Net.NFSe.Demo
             if (InputBox.Show("Item na lista de serviço", "Informe o item na lista de serviço.", ref itemListaServico).Equals(DialogResult.Cancel)) return;
 
             // Setar o cnae de acordo com o schema aceito pelo provedor.
-            var cnae = municipio.Provedor.IsIn(NFSeProvider.SIAPNet, NFSeProvider.Sintese, NFSeProvider.ABase) ? "5211701" : "861010101";
+            var cnae = municipio.Provedor.IsIn(NFSeProvider.SIAPNet, NFSeProvider.Sintese, NFSeProvider.ABase, NFSeProvider.Pronim203) ? "5211701" : "861010101";
             if (InputBox.Show("CNAE", "Informe o codigo CNAE.", ref cnae).Equals(DialogResult.Cancel)) return;
             nfSe.Servico.CodigoCnae = cnae;
 
