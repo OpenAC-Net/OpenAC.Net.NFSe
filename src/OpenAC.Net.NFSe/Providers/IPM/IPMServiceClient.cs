e26b5b7e7130737f75236f6867dce37f08b0bc08
Rafael Teno Dias <rfteno@gmail.com>
2023-04-02 13:12:48 -0400

    [-] Ajuste do copyrigths dos arquivos.

--- src/OpenAC.Net.NFSe/Providers/IPM/IPMServiceClient.cs
+++ src/OpenAC.Net.NFSe/Providers/IPM/IPMServiceClient.cs
@@ -9,7 +9,7 @@
 // ***********************************************************************
 // <copyright file="IPMServiceClient.cs" company="OpenAC .Net">
 //		        		   The MIT License (MIT)
-//	     		    Copyright (c) 2014 - 2022 Projeto OpenAC .Net
+//	     		    Copyright (c) 2014 - 2023 Projeto OpenAC .Net
 //
 //	 Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the "Software"),
