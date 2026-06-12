d1635588b5347bb95e6e120ef71598cedb152e1b
Fabio Nobre <fabiohcnobre@hotmail.com>
2026-06-12 10:00:21 -0300

    Refactor NFSe cancellation preparation and signing
    Mensagem : Erro em CancelarNFSe: Erro ao efetuar assinatura digital.

--- src/OpenAC.Net.NFSe/Providers/NFeCidades/ProviderNFeCidades.cs
+++ src/OpenAC.Net.NFSe/Providers/NFeCidades/ProviderNFeCidades.cs
@@ -277,32 +277,37 @@ internal sealed class ProviderNFeCidades : ProviderABRASF201
     }
 
     protected override void PrepararCancelarNFSe(RetornoCancelar retornoWebservice)
-{
-    if (retornoWebservice.NumeroNFSe.IsEmpty() || retornoWebservice.CodigoCancelamento.IsEmpty())
     {
-        retornoWebservice.Erros.Add(new EventoRetorno { Codigo = "0", Descricao = "Número da NFSe/Codigo de cancelamento não informado para cancelamento." });
-        return;
+        if (retornoWebservice.NumeroNFSe.IsEmpty() || retornoWebservice.CodigoCancelamento.IsEmpty())
+        {
+            retornoWebservice.Erros.Add(new EventoRetorno { Codigo = "0", Descricao = "Número da NFSe/Codigo de cancelamento não informado para cancelamento." });
+            return;
+        }
+    
+        var loteBuilder = new StringBuilder();
+        loteBuilder.Append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+        loteBuilder.Append($"<CancelarNfseEnvio {GetNamespace()}>");
+        loteBuilder.Append("<Pedido>");
+        loteBuilder.Append($"<InfPedidoCancelamento Id=\"N{retornoWebservice.NumeroNFSe}\">");
+        loteBuilder.Append("<IdentificacaoNfse>");
+        loteBuilder.Append($"<Numero>{retornoWebservice.NumeroNFSe}</Numero>");
+        loteBuilder.Append($"<CpfCnpj><Cnpj>{Configuracoes.PrestadorPadrao.CpfCnpj.ZeroFill(14)}</Cnpj></CpfCnpj>");
+        loteBuilder.Append($"<InscricaoMunicipal>{Configuracoes.PrestadorPadrao.InscricaoMunicipal}</InscricaoMunicipal>");
+        loteBuilder.Append($"<CodigoMunicipio>{Configuracoes.PrestadorPadrao.Endereco.CodigoMunicipio}</CodigoMunicipio>");
+        loteBuilder.Append("</IdentificacaoNfse>");
+        loteBuilder.Append($"<CodigoCancelamento>{retornoWebservice.CodigoCancelamento}</CodigoCancelamento>");
+        loteBuilder.Append($"<CMotivo>{retornoWebservice.CodigoCancelamento}</CMotivo>");
+        loteBuilder.Append($"<XMotivo>{retornoWebservice.Motivo}</XMotivo>");
+        loteBuilder.Append("</InfPedidoCancelamento>");
+        loteBuilder.Append("</Pedido>");
+        loteBuilder.Append("</CancelarNfseEnvio>");
+        retornoWebservice.XmlEnvio = loteBuilder.ToString();
     }
 
-    var loteBuilder = new StringBuilder();
-    loteBuilder.Append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-    loteBuilder.Append($"<CancelarNfseEnvio {GetNamespace()}>");
-    loteBuilder.Append("<Pedido>");
-    loteBuilder.Append($"<InfPedidoCancelamento Id=\"N{retornoWebservice.NumeroNFSe}\">");
-    loteBuilder.Append("<IdentificacaoNfse>");
-    loteBuilder.Append($"<Numero>{retornoWebservice.NumeroNFSe}</Numero>");
-    loteBuilder.Append($"<CpfCnpj><Cnpj>{Configuracoes.PrestadorPadrao.CpfCnpj.ZeroFill(14)}</Cnpj></CpfCnpj>");
-    loteBuilder.Append($"<InscricaoMunicipal>{Configuracoes.PrestadorPadrao.InscricaoMunicipal}</InscricaoMunicipal>");
-    loteBuilder.Append($"<CodigoMunicipio>{Configuracoes.PrestadorPadrao.Endereco.CodigoMunicipio}</CodigoMunicipio>");
-    loteBuilder.Append("</IdentificacaoNfse>");
-    loteBuilder.Append($"<CodigoCancelamento>{retornoWebservice.CodigoCancelamento}</CodigoCancelamento>");
-    loteBuilder.Append($"<CMotivo>{retornoWebservice.CodigoCancelamento}</CMotivo>");
-    loteBuilder.Append($"<XMotivo>{retornoWebservice.Motivo}</CMotivo>");
-    loteBuilder.Append("</InfPedidoCancelamento>");
-    loteBuilder.Append("</Pedido>");
-    loteBuilder.Append("</CancelarNfseEnvio>");
-    retornoWebservice.XmlEnvio = loteBuilder.ToString();
-}
+     protected override void AssinarCancelarNFSe(RetornoCancelar retornoWebservice)
+     {
+        retornoWebservice.XmlEnvio = XmlSigning.AssinarXml(retornoWebservice.XmlEnvio, "Pedido", "", Certificado); 
+     }
 
     protected override IServiceClient GetClient(TipoUrl tipo) => new NFeCidadesServiceClient(this, tipo);
 
